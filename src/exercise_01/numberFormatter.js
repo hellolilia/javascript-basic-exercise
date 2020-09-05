1a9f571efabbea80287e1d22ce35a683acfeaf0a
hellolilia <70508924+hellolilia@users.noreply.github.com>
2020-09-05 22:33:50 +0800

    fix numberFormatter test

--- src/exercise_01/numberFormatter.js
+++ src/exercise_01/numberFormatter.js
@@ -7,6 +7,14 @@ export default function formatNumber(number, option) {
   //
   // * Please implement the function and pass all the tests in format_number_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  if (option === undefined) {
+    option = {};
+  }
+  let numfix;
+  if (option.currency === true) {
+    numfix = '$ '+ number.toFixed(2);
+  } else {
+    numfix = number.toFixed(2);
+  }
+  return numfix;
 }
