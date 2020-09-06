bd3bd89c1bcdcbce9c0c95821482ca051d94bc08
hellolilia <70508924+hellolilia@users.noreply.github.com>
2020-09-06 16:16:20 +0800

    fix arrayToList test

--- src/exercise_04/arrayToList.js
+++ src/exercise_04/arrayToList.js
@@ -6,5 +6,20 @@ export default function arrayToList(array) {
   // * Please implement this function and pass all the tests in array_to_list_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  let list;
+  let oldlist;
+  if (array == undefined || null) {
+    throw new Error('Creating list from undefined array');
+  } else if (array.length == 0) {
+    throw new Error('Creating list from empty array');
+  } else if (array.length == 1) {
+    list = { value: array[0], next: null };
+  } else {
+    list = { value: array[array.length - 1], next: null };
+    for (let i = array.length - 2; i >= 0; i -= 1) {
+      oldlist = list;
+      list = { value: array[i], next: oldlist };
+    }
+  }
+  return list;
 }
