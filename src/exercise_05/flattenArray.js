ecf20556780613868735d0ec37590cfd0cc48939
hellolilia <70508924+hellolilia@users.noreply.github.com>
2020-09-06 17:15:01 +0800

    fix flattenArray test

--- src/exercise_05/flattenArray.js
+++ src/exercise_05/flattenArray.js
@@ -5,7 +5,6 @@ export default function flattenArray(array) {
   //
   // * Please implement this function and pass all the tests in flatten_array_spec.js.
   // * Please do NOT modify the signature of the function.
-
   let flatA;
   if (array == undefined || null) {
     throw new Error('Flatten undefined or null array');
