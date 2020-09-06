b2ffc46a159156954b6e10da300d9dd49160ac90
hellolilia <70508924+hellolilia@users.noreply.github.com>
2020-09-06 17:13:32 +0800

    flattenArray test

--- src/exercise_05/flattenArray.js
+++ src/exercise_05/flattenArray.js
@@ -6,5 +6,13 @@ export default function flattenArray(array) {
   // * Please implement this function and pass all the tests in flatten_array_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  let flatA;
+  if (array == undefined || null) {
+    throw new Error('Flatten undefined or null array');
+  } else if (array.length == 0) {
+    flatA = [];
+  } else {
+    flatA = [].concat.apply([],array);
+  }
+  return flatA;
 }
